b49171fcd0b2d221c0312dac55f612f54e07ab02
Pedro Soares <ngrpedro22@gmail.com>
2022-12-07 17:54:46 -0300

    partners init + css ajuste

---
+++ src/sections/Partners/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./index.module.sass";
+
+type Props = {};
+
+const index = (props: Props) => {
+  return <div className={styles.container}>index</div>;
+};
+
+export default index;

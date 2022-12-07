98f6c0da42351a4f6ffba7768b3bb83f48b16e30
Pedro Soares <ngrpedro22@gmail.com>
2022-12-07 19:13:31 -0300

    footer styling

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -1,11 +1,28 @@
 import styles from "./index.module.sass";
+import { InstagramLogo, WhatsappLogo, EnvelopeSimple } from "phosphor-react";
 
 type Props = {};
 
 const index = (props: Props) => {
   return (
-    <div className={styles.container}>
-      <h1>Footer</h1>
+    <div className={styles.box}>
+      <div className={styles.container}>
+        <h1>Wellcome</h1>
+
+        <div>
+          <ul className="flex items-center justify-center gap-4">
+            <li>
+              <InstagramLogo size={32} color="white" />
+            </li>
+            <li>
+              <WhatsappLogo size={32} color="white" />
+            </li>
+            <li>
+              <EnvelopeSimple size={32} color="white" />
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };

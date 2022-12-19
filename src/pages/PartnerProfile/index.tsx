2417b66342f7c6124804dbaf9c45be5407ad5e13
Pedro Soares <ngrpedro22@gmail.com>
2022-12-19 12:03:08 -0300

    user profile

--- src/pages/PartnerProfile/index.tsx
+++ src/pages/PartnerProfile/index.tsx
@@ -1,4 +1,4 @@
-import { Avatar } from "@chakra-ui/react";
+import { Avatar, Divider } from "@chakra-ui/react";
 import {
   Envelope,
   FacebookLogo,
@@ -16,38 +16,97 @@ const index = () => {
 
       {/* grid */}
       <div className={styles.gridContent}>
-        {/* user sidebar  */}
-        <div className={styles.userSideBar}>
-          <div>
-            <Avatar size="2xl" />
-            <div className={styles.sidebarTitle}>
-              <h1>Espaço Santo Agostinho</h1>
-              <span>Endereço xxx - XX</span>
+        <div>
+          {/* user sidebar  */}
+          <div className={styles.userSideBar}>
+            <div>
+              <Avatar size="2xl" />
+              <div className={styles.sidebarTitle}>
+                <h1>Espaço Santo Agostinho</h1>
+                <span>Endereço xxx - XX</span>
+              </div>
             </div>
-          </div>
 
-          <div className={styles.area}>
-            <span>Espaço de acolhimento</span>
-          </div>
+            <div className={styles.area}>
+              <span>Espaço de acolhimento</span>
+            </div>
 
-          <div className={styles.starts}>
-            <Star size={26} />
-            <Star size={26} />
-            <Star size={26} />
-            <Star size={26} />
-            <Star size={26} />
-          </div>
+            <div className={styles.starts}>
+              <Star size={26} />
+              <Star size={26} />
+              <Star size={26} />
+              <Star size={26} />
+              <Star size={26} />
+            </div>
 
-          <div className={styles.sidebarSocialMedias}>
-            <InstagramLogo size={32} />
-            <FacebookLogo size={32} />
-            <WhatsappLogo size={32} />
-            <Envelope size={32} />
+            <div className={styles.sidebarSocialMedias}>
+              <InstagramLogo size={32} />
+              <FacebookLogo size={32} />
+              <WhatsappLogo size={32} />
+              <Envelope size={32} />
+            </div>
           </div>
         </div>
 
         {/* user main content */}
-        <div></div>
+        <div className={styles.mainContent}>
+          {/* tags */}
+          <div className={styles.userTags}>
+            <div className={styles.tag}>
+              <span>Espaço de acolhimento</span>
+            </div>
+            <div className={styles.tag}>
+              <span>LGBTQIAP+</span>
+            </div>
+            <div className={styles.tag}>
+              <span>Adultos</span>
+            </div>
+            <div className={styles.tag}>
+              <span>Mulheres</span>
+            </div>
+          </div>
+          <Divider />
+
+          {/* bio */}
+          <div className={styles.bio}>
+            <h1>Biografia</h1>
+            <div className={styles.texts}>
+              <div className={styles.text}>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias
+                repellendus quisquam voluptates reiciendis rerum, quas laborum
+                recusandae facilis placeat nequeut.
+              </div>
+              <div className={styles.text}>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias
+                repellendus quisquam voluptates reiciendis rerum, quas laborum
+                recusandae facilis placeat neque temporibus molestiae pariatur
+                dolorum at, nobis eos labore illo ut. Temporibus molestiae
+                pariatur dolorum at, nobis eos labore illo .
+              </div>
+              <div className={styles.text}>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias
+                repellendus quisquam voluptates reiciendis rerum, quas laborum
+                recusandae facilis placeat neque temporibus molestiae pariatur
+                dolorum at, nobis eos labore illo ut. Endis rerum, quas laborum
+                recusandae facilis placeat neque temporibus molestiae pariatur
+                dolorum at, nobis eos labore illo ut.
+              </div>
+            </div>
+          </div>
+          <Divider />
+
+          {/* galeria */}
+          <div className={styles.galery}>
+            <div className={styles.galeryFirstImage}></div>
+            <div className={styles.galeryMinis}>
+              <div className={styles.galeryMini}></div>
+              <div className={styles.galeryMini}></div>
+              <div className={styles.galeryMini}></div>
+              <div className={styles.galeryMini}></div>
+            </div>
+          </div>
+          <Divider />
+        </div>
       </div>
     </div>
   );
